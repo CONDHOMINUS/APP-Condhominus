6f0460d12597304cce026460eff847a27c35fd14
Gabi <mzu7i87mgs0660Gol!>
2023-11-22 13:27:28 -0300

    Bloqueio do tema dark

---
+++ CondhominusApp/app/build/generated/source/buildConfig/androidTest/debug/com/example/condhominus/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.condhominus.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.condhominus.test";
+  public static final String BUILD_TYPE = "debug";
+}
